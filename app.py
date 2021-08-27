824d5b1ff107f0e2e47fdda9a43887cae6699161
Psycab <10110e@naver.com>
2021-08-27 16:48:24 +0900

    dockerfile and host setting

--- app.py
+++ app.py
@@ -55,5 +55,6 @@ def update(id):
     else:
         return render_template('update.html', task=task)
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8000)
